559781466027c560eafdaf237ca65453805833db
Alexandre Beck <alexandrebeck2003@gmail.com>
2024-05-15 18:44:40 -0300

    calculating final result

--- src/components/FirstModule/firstModule.tsx
+++ src/components/FirstModule/firstModule.tsx
@@ -5,17 +5,38 @@ import { z } from "zod";
 import { Button } from "../ui/button";
 import { Card, CardContent, CardHeader } from "../ui/card";
 import { Checkbox } from "../ui/checkbox";
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "../ui/form";
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "../ui/form";
 import { Separator } from "../ui/separator";
 
 const formSchema = z.object({
-    item1: z.array(z.string()).nonempty({ message: "Select at least one option" }),
-    item2: z.array(z.string()).nonempty({ message: "Select at least one option" }),
-    item3: z.array(z.string()).nonempty({ message: "Select at least one option" }),
-    item4: z.array(z.string()).nonempty({ message: "Select at least one option" }),
-    item5: z.array(z.string()).nonempty({ message: "Select at least one option" }),
-    item6: z.array(z.string()).nonempty({ message: "Select at least one option" }),
-    item7: z.array(z.string()).nonempty({ message: "Select at least one option" }),
+    item1: z
+        .array(z.string())
+        .nonempty({ message: "Select at least one option" }),
+    item2: z
+        .array(z.string())
+        .nonempty({ message: "Select at least one option" }),
+    item3: z
+        .array(z.string())
+        .nonempty({ message: "Select at least one option" }),
+    item4: z
+        .array(z.string())
+        .nonempty({ message: "Select at least one option" }),
+    item5: z
+        .array(z.string())
+        .nonempty({ message: "Select at least one option" }),
+    item6: z
+        .array(z.string())
+        .nonempty({ message: "Select at least one option" }),
+    item7: z
+        .array(z.string())
+        .nonempty({ message: "Select at least one option" }),
 });
 
 type FormData = z.infer<typeof formSchema>;
@@ -27,17 +48,17 @@ const items = {
         "1.3 -Gestores",
         "1.4- Equipes Técnicas",
         "1.5- Famílias dos Estudantes",
-        "1.6 - Suportes Administrativos"
+        "1.6 - Suportes Administrativos",
     ],
     item2: [
-        "2.1- Levantamento e mapeamento dos profissionais da educação com deficiência e Transtorno do Espectro Autista"
+        "2.1- Levantamento e mapeamento dos profissionais da educação com deficiência e Transtorno do Espectro Autista",
     ],
     item3: [
-        "3.1- Levantamento e mapeamento dos profissionais da educação que atuam em Escolas do Campo, Educação Bilíngue, Escolas Indígenas, Educação Especial"
+        "3.1- Levantamento e mapeamento dos profissionais da educação que atuam em Escolas do Campo, Educação Bilíngue, Escolas Indígenas, Educação Especial",
     ],
     item4: [
         "4.1- Processo de alfabetização (1° e 2° anos do Ensino Fundamental)",
-        "4.2 - Recomposição das aprendizagens (3°, 4° e 5° anos do Ensino Fundamental)"
+        "4.2 - Recomposição das aprendizagens (3°, 4° e 5° anos do Ensino Fundamental)",
     ],
     item5: [
         "5.1- Sinaliza uma proposta pedagógica na perspectiva interdisciplinar",
@@ -46,60 +67,105 @@ const items = {
         "5.4- Sinalizam discussões e proposições no âmbito da Educação Especial",
         "5.5 – Sinalizam discussões e proposições no âmbito da Educação do Campo",
         "5.6 – Sinalizam discussões no âmbito da Educação Indígena",
-        "Não se aplica"
+        "Não se aplica",
     ],
     item6: [
         "6.1- Dentro da carga horária de trabalho",
         "6.2- Dentro de 1/3 da carga horária (coordenações, hora de atividades, planejamentos e reuniões)",
         "6.3- Sábados",
-        "6.4 – Contraturno (para além da carga horária)"
+        "6.4 – Contraturno (para além da carga horária)",
     ],
     item7: [
         "7.1- 100%",
         "7.2- Entre 80 a 99%",
         "7.3- Entre 60 a 79%",
-        "7.4 - Até 59%"
-    ]
+        "7.4 - Até 59%",
+    ],
 };
 
-const calculateScore = (itemName: keyof FormData, selectedOptions: string[]) => {
+const calculateScore = (
+    itemName: keyof FormData,
+    selectedOptions: string[]
+) => {
     switch (itemName) {
-        case 'item1':
+        case "item1":
             if (selectedOptions.length === 1) return 0;
             if (selectedOptions.length === 2) return 0.25;
             if (selectedOptions.length === 3) return 0.5;
             if (selectedOptions.length === 4) return 0.75;
             if (selectedOptions.length >= 5) return 1;
             return 0;
-        case 'item2':
+        case "item2":
             return selectedOptions.length > 0 ? 1 : 0;
-        case 'item3':
+        case "item3":
             return selectedOptions.length > 0 ? 1 : 0;
-        case 'item4':
-            if (selectedOptions.includes("4.1- Processo de alfabetização (1° e 2° anos do Ensino Fundamental)") &&
-                selectedOptions.includes("4.2 - Recomposição das aprendizagens (3°, 4° e 5° anos do Ensino Fundamental)")) return 1;
-            if (selectedOptions.includes("4.1- Processo de alfabetização (1° e 2° anos do Ensino Fundamental)")) return 0.75;
-            if (selectedOptions.includes("4.2 - Recomposição das aprendizagens (3°, 4° e 5° anos do Ensino Fundamental)")) return 0.5;
+        case "item4":
+            if (
+                selectedOptions.includes(
+                    "4.1- Processo de alfabetização (1° e 2° anos do Ensino Fundamental)"
+                ) &&
+                selectedOptions.includes(
+                    "4.2 - Recomposição das aprendizagens (3°, 4° e 5° anos do Ensino Fundamental)"
+                )
+            )
+                return 1;
+            if (
+                selectedOptions.includes(
+                    "4.1- Processo de alfabetização (1° e 2° anos do Ensino Fundamental)"
+                )
+            )
+                return 0.75;
+            if (
+                selectedOptions.includes(
+                    "4.2 - Recomposição das aprendizagens (3°, 4° e 5° anos do Ensino Fundamental)"
+                )
+            )
+                return 0.5;
             return 0;
-        case 'item5':
+        case "item5":
             if (selectedOptions.includes("Não se aplica")) return 1;
             if (selectedOptions.length === 6) return 1;
             if (selectedOptions.length === 0) return 0;
-            if (selectedOptions.includes("5.1- Sinaliza uma proposta pedagógica na perspectiva interdisciplinar") &&
-                selectedOptions.includes("5.2- Contempla o bloco de conteúdo Número") && selectedOptions.length === 2) return 0.5;
-            if (selectedOptions.includes("5.1- Sinaliza uma proposta pedagógica na perspectiva interdisciplinar") &&
+            if (
+                selectedOptions.includes(
+                    "5.1- Sinaliza uma proposta pedagógica na perspectiva interdisciplinar"
+                ) &&
                 selectedOptions.includes("5.2- Contempla o bloco de conteúdo Número") &&
-                selectedOptions.includes("5.3- Contemplam os blocos de conteúdo: Números, Álgebra, Geometria, Grandezas e Medidas e Probabilidade e Estatística")) return 0.75;
-            if (selectedOptions.includes("5.2- Contempla o bloco de conteúdo Número")) return 0.25;
+                selectedOptions.length === 2
+            )
+                return 0.5;
+            if (
+                selectedOptions.includes(
+                    "5.1- Sinaliza uma proposta pedagógica na perspectiva interdisciplinar"
+                ) &&
+                selectedOptions.includes("5.2- Contempla o bloco de conteúdo Número") &&
+                selectedOptions.includes(
+                    "5.3- Contemplam os blocos de conteúdo: Números, Álgebra, Geometria, Grandezas e Medidas e Probabilidade e Estatística"
+                )
+            )
+                return 0.75;
+            if (selectedOptions.includes("5.2- Contempla o bloco de conteúdo Número"))
+                return 0.25;
             return 0;
-        case 'item6':
+        case "item6":
             if (selectedOptions.length === 0) return 0;
             if (selectedOptions.includes("6.3- Sábados")) return 0.25;
-            if (selectedOptions.includes("6.4 – Contraturno (para além da carga horária)")) return 0.5;
-            if (selectedOptions.includes("6.2- Dentro de 1/3 da carga horária (coordenações, hora de atividades, planejamentos e reuniões)")) return 0.75;
-            if (selectedOptions.includes("6.1- Dentro da carga horária de trabalho")) return 1;
+            if (
+                selectedOptions.includes(
+                    "6.4 – Contraturno (para além da carga horária)"
+                )
+            )
+                return 0.5;
+            if (
+                selectedOptions.includes(
+                    "6.2- Dentro de 1/3 da carga horária (coordenações, hora de atividades, planejamentos e reuniões)"
+                )
+            )
+                return 0.75;
+            if (selectedOptions.includes("6.1- Dentro da carga horária de trabalho"))
+                return 1;
             return 0;
-        case 'item7':
+        case "item7":
             if (selectedOptions.includes("7.1- 100%")) return 1;
             if (selectedOptions.includes("7.2- Entre 80 a 99%")) return 0.75;
             if (selectedOptions.includes("7.3- Entre 60 a 79%")) return 0.5;
@@ -118,6 +184,7 @@ export function FirstModule() {
     const [scoreItem5, setScoreItem5] = useState(0);
     const [scoreItem6, setScoreItem6] = useState(0);
     const [scoreItem7, setScoreItem7] = useState(0);
+    const [finalResult, setFinalResut] = useState(0);
 
     const form = useForm<FormData>({
         resolver: zodResolver(formSchema),
@@ -133,19 +200,28 @@ export function FirstModule() {
     });
 
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const handleCheckboxChange = (field: any, itemName: keyof FormData, value: string) => {
+    const handleCheckboxChange = (
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        field: any,
+        itemName: keyof FormData,
+        value: string
+    ) => {
         return (checked: boolean) => {
             let newValue;
 
-            if (itemName === 'item5' && value === 'Não se aplica') {
+            if (itemName === "item5" && value === "Não se aplica") {
                 newValue = checked ? [value] : [];
             } else {
                 newValue = checked
                     ? [...field.value, value]
                     : field.value.filter((v: string) => v !== value);
 
-                if (itemName === 'item5' && newValue.includes('Não se aplica')) {
-                    newValue = ['Não se aplica'];
+                if (itemName === "item5" && newValue.includes("Não se aplica")) {
+                    newValue = ["Não se aplica"];
+                }
+
+                if (itemName === "item7") {
+                    newValue = [value];
                 }
             }
 
@@ -154,25 +230,25 @@ export function FirstModule() {
             const score = calculateScore(itemName, newValue);
 
             switch (itemName) {
-                case 'item1':
+                case "item1":
                     setScoreItem1(score);
                     break;
-                case 'item2':
+                case "item2":
                     setScoreItem2(score);
                     break;
-                case 'item3':
+                case "item3":
                     setScoreItem3(score);
                     break;
-                case 'item4':
+                case "item4":
                     setScoreItem4(score);
                     break;
-                case 'item5':
+                case "item5":
                     setScoreItem5(score);
                     break;
-                case 'item6':
+                case "item6":
                     setScoreItem6(score);
                     break;
-                case 'item7':
+                case "item7":
                     setScoreItem7(score);
                     break;
                 default:
@@ -181,19 +257,18 @@ export function FirstModule() {
         };
     };
 
-    function onSubmit(values: FormData) {
-        // Do something with the form values.
-        // ✅ This will be type-safe and validated.
-        console.log(values);
-        console.log({
-            scoreItem1,
-            scoreItem2,
-            scoreItem3,
-            scoreItem4,
-            scoreItem5,
-            scoreItem6,
-            scoreItem7
-        });
+    function onSubmit() {
+        setFinalResut(
+            (
+                scoreItem1 +
+                scoreItem2 +
+                scoreItem3 +
+                scoreItem4 +
+                scoreItem5 +
+                scoreItem6 +
+                scoreItem7
+            ) / 7
+        );
     }
 
     return (
@@ -201,7 +276,10 @@ export function FirstModule() {
             <CardHeader />
             <CardContent>
                 <Form {...form}>
-                    <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-4">
+                    <form
+                        onSubmit={form.handleSubmit(onSubmit)}
+                        className="flex flex-col gap-4"
+                    >
                         {Object.keys(formSchema.shape).map((itemName, index) => (
                             <FormField
                                 key={itemName}
@@ -209,33 +287,43 @@ export function FirstModule() {
                                 name={itemName as keyof FormData}
                                 render={({ field }) => (
                                     <FormItem className="justify-center flex flex-col">
-                                        <FormLabel>Item {index + 1} - Score: {(() => {
-                                            switch (itemName) {
-                                                case 'item1':
-                                                    return scoreItem1;
-                                                case 'item2':
-                                                    return scoreItem2;
-                                                case 'item3':
-                                                    return scoreItem3;
-                                                case 'item4':
-                                                    return scoreItem4;
-                                                case 'item5':
-                                                    return scoreItem5;
-                                                case 'item6':
-                                                    return scoreItem6;
-                                                case 'item7':
-                                                    return scoreItem7;
-                                                default:
-                                                    return 0;
-                                            }
-                                        })()}</FormLabel>
+                                        <FormLabel>
+                                            Item {index + 1} - Score:{" "}
+                                            {(() => {
+                                                switch (itemName) {
+                                                    case "item1":
+                                                        return scoreItem1;
+                                                    case "item2":
+                                                        return scoreItem2;
+                                                    case "item3":
+                                                        return scoreItem3;
+                                                    case "item4":
+                                                        return scoreItem4;
+                                                    case "item5":
+                                                        return scoreItem5;
+                                                    case "item6":
+                                                        return scoreItem6;
+                                                    case "item7":
+                                                        return scoreItem7;
+                                                    default:
+                                                        return 0;
+                                                }
+                                            })()}
+                                        </FormLabel>
                                         <FormControl>
                                             <div className="flex flex-col gap-2">
-                                                {items[itemName as keyof FormData].map(value => (
-                                                    <label key={value} className="flex items-center space-x-2">
+                                                {items[itemName as keyof FormData].map((value) => (
+                                                    <label
+                                                        key={value}
+                                                        className="flex items-center space-x-2"
+                                                    >
                                                         <Checkbox
                                                             checked={field.value.includes(value)}
-                                                            onCheckedChange={handleCheckboxChange(field, itemName as keyof FormData, value)}
+                                                            onCheckedChange={handleCheckboxChange(
+                                                                field,
+                                                                itemName as keyof FormData,
+                                                                value
+                                                            )}
                                                         />
                                                         <span>{value}</span>
                                                     </label>
@@ -248,7 +336,8 @@ export function FirstModule() {
                                 )}
                             />
                         ))}
-                        <Button type="submit">Submit</Button>
+                        <Button type="submit">Calcular</Button>
+                        {finalResult !== 0 && <div>{`Resultado final: ${finalResult}`}</div>}
                     </form>
                 </Form>
             </CardContent>
