d2311490ff58df3e74cdcc6a1bca73f5b62b4f35
Alexandre Beck <alexandrebeck2003@gmail.com>
2024-05-15 11:17:43 -0300

    added first form structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,58 @@
-import { Card } from "./components/ui/card"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import { Button } from "./components/ui/button"
+import { Card, CardContent, CardHeader } from "./components/ui/card"
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "./components/ui/form"
+import { Input } from "./components/ui/input"
+
+const formSchema = z.object({
+  username: z.string().min(2).max(50),
+})
 
 function App() {
 
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  })
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+  }
+
+
   return (
-    <main>
-      <Card>
-        teste
+    <main className="flex justify-center items-center w-screen h-screen">
+      <Card className="w-2/4 h-auto min-h-[50vh]">
+        <CardHeader />
+        <CardContent>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Username</FormLabel>
+                    <FormControl>
+                      <Input placeholder="shadcn" {...field} />
+                    </FormControl>
+                    <FormDescription>
+                      This is your public display name.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit">Submit</Button>
+            </form>
+          </Form>
+        </CardContent>
       </Card>
     </main>
   )
