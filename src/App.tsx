6cdc69185ab93dc26d14042b39a83f57e93835d4
Alexandre Beck <alexandrebeck2003@gmail.com>
2024-05-15 11:57:16 -0300

    created module forms structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,30 @@
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
-import { Button } from "./components/ui/button"
-import { Card, CardContent, CardHeader } from "./components/ui/card"
-import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "./components/ui/form"
-import { Input } from "./components/ui/input"
+import { FirstModule } from "./components/FirstModule/firstModule"
+import { SecondModule } from "./components/SecondModule/secondModule"
+import { ThirdModule } from "./components/ThirdModule/thirdModule"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "./components/ui/tabs"
 
-const formSchema = z.object({
-  username: z.string().min(2).max(50),
-})
+
 
 function App() {
 
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      username: "",
-    },
-  })
-
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values)
-  }
-
-
   return (
-    <main className="flex justify-center items-center w-screen h-screen">
-      <Card className="w-2/4 h-auto min-h-[50vh]">
-        <CardHeader />
-        <CardContent>
-          <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-              <FormField
-                control={form.control}
-                name="username"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Username</FormLabel>
-                    <FormControl>
-                      <Input placeholder="shadcn" {...field} />
-                    </FormControl>
-                    <FormDescription>
-                      This is your public display name.
-                    </FormDescription>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <Button type="submit">Submit</Button>
-            </form>
-          </Form>
-        </CardContent>
-      </Card>
+    <main className="flex justify-center items-center w-screen min-h-screen py-10">
+      <Tabs defaultValue="firstModule" className="w-fit">
+        <TabsList className="flex gap-4 w-full">
+          <TabsTrigger className="w-1/3" value="firstModule">First Module</TabsTrigger>
+          <TabsTrigger className="w-1/3" value="secondModule">Second Module</TabsTrigger>
+          <TabsTrigger className="w-1/3" value="thirdModule">Third Module</TabsTrigger>
+        </TabsList>
+        <TabsContent value="firstModule">
+          <FirstModule />
+        </TabsContent>
+        <TabsContent value="secondModule">
+          <SecondModule />
+        </TabsContent>
+        <TabsContent value="thirdModule">
+          <ThirdModule />
+        </TabsContent>
+      </Tabs>
     </main>
   )
 }
