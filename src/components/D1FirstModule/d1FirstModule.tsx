f87f09485dcc0617739229f9631df36854a04a6c
Alexandre Beck <alexandrebeck2003@gmail.com>
2024-05-17 00:39:43 -0300

    add gap to pdf

--- src/components/D1FirstModule/d1FirstModule.tsx
+++ src/components/D1FirstModule/d1FirstModule.tsx
@@ -322,7 +322,7 @@ export function D1FirstModule() {
                 <Form {...form}>
                     <form
                         onSubmit={form.handleSubmit(onSubmit)}
-                        className="flex flex-col gap-4"
+                        className="flex flex-col gap-4" p-4
                         ref={pdfRef}
                     >
                         {Object.keys(formSchema.shape).map((itemName) => (
@@ -392,7 +392,7 @@ export function D1FirstModule() {
                             <Button type="submit">Calcular</Button>
                         )}
                         {isLoading ? (
-                            <div className="flex flex-col gap-4">
+                            <div className="flex flex-col gap-4" p-4>
                                 <Skeleton className="w-1/6 h-8" />
                                 <Skeleton className="w-1/6 h-8" />
                                 <Skeleton className="w-1/6 h-8" />
